3da6c5bbc9cba95e71c8ad101a82c8fc0d46e0b7
Ishayu Ghosh <ishayu.ghosh@gmail.com>
2023-02-22 21:56:28 +0400

    second tutorial

--- website/auth.py
+++ website/auth.py
@@ -1,35 +1,58 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for,request, flash
+from . import db
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth= Blueprint("auth",__name__)
 
-@auth.route("/login")
+
+
+@auth.route("/login", methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Password is incorrect", category="error")
+        else:
+            flash("Email does not exist", category="error")
     return render_template("login.html")
 
-@auth.route("/sign_up")
+@auth.route("/sign_up", methods=["GET","POST"])
 def sign_up():
+    if request.method == "POST": 
+        email = request.form.get("email")
+        password = request.form.get("password")
+        password1 = request.form.get("password1")
+
+        email_exists = User.query.filter_by(email=email).first()
+        if email_exists:
+            flash("Email is already in use", category="error")
+        elif password != password1:
+            flash("Passwords do not match", category="error")
+        elif len(password) <8:
+            flash("Password must be at least 8 characters", category="error")
+        else:
+            new_user = User(email=email, password= generate_password_hash(password, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            flash("User created")
+            login_user(new_user, remember=True)
+            return redirect(url_for("views.home"))
     return render_template("sign_up.html")
 
+
 @auth.route("/sign_out")
+@login_required 
 def sign_out():
+    logout_user()
     return redirect(url_for("views.home"))
 
-# #
-#  <nav class = "navbar navbar-expanded-lg navbar-dark bg-dark">
-#     <div class ="container-fluid">
-#       <button 
-#       class="navbar-toggler"
-#       type="button"
-#       data-bs-toggle="collapse" 
-#       data-bs-target="#navbar"
-#       >
-#         <span class="navbar-toggler-icon"></span>
-#       </button>
-#       <div class="collapse navbar-collapse" id="navbar">
-#         <div class="navbar-nav">
-#           <a class="nav-item nav-link" href="/home">Home</a>
-#           <a class="nav-item nav-link" href="/login">Login</a>
-#           <a class="nav-item nav-link" href="/sign_up">Sign Up</a>
-#         </div>
-#     </div>
-#   </nav>
+
